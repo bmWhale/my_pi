8079905421445bc18f1e9ac0cd70b9050173718a
hugh712 <hugh712@gmail.com>
2017-01-09 08:52:19 +0000

    update my_wifi_ap

--- my_wifi_ap/setup.sh
+++ my_wifi_ap/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -e
+
 #1. install udhcpd and hostapd
 sudo apt-get update
 sudo apt-get install udhcpd
 sudo apt-get install hostapd
 
 #2. copy all config to /
-sudo cp TOP/* /
+sudo cp -irf TOP/* /
 
 #3. change priviledge
-sudo chmod +x iptables
+sudo chmod +x /etc/init.d/iptables
 
 #4. execute when boot up
 sudo update-rc.d iptables defaults
